3658ae7ea9df8a225fb30c994582a5d5b03bb11f
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-04-26 15:51:28 +0600

    feat: ruff added

--- data/class_10/math/real_numbers.py
+++ data/class_10/math/real_numbers.py
@@ -1,18 +1,22 @@
-from domain import BasePage
-from public.resources.class_10.math.real_numbers import RealNumbers as numberCategorize
 import streamlit as st
+
+from domain import BasePage
 from domain.services import GraphVisualize
+from public.resources.class_10.math.real_numbers import RealNumbers as numberCategorize
 
 
 class RealNumbers(BasePage):
     def __init__(self):
         super().__init__(file_location=__file__)
+
     def build_page(self, **args):
         pass
-        
+
+
 realNumbers = RealNumbers()
 categorizer = numberCategorize()
 categories = categorizer.getCategories
+finalCategories = categorizer.getFinalCategories
 graph = categorizer.getGraph
 
 
@@ -22,21 +26,21 @@ number = st.number_input("Enter a number")
 
 selected = st.pills(
     label="এটা কোন ধরনের বাস্তব সংখ্যা?",
-    options=categories.keys(),
+    options=finalCategories,
     selection_mode="single",
 )
 
 
 if selected:
-    st.write(f"আপনি {selected} ক্যাটেগরি নির্বাচন করেছেন।")
+    st.write(f"আপনি `{selected}` ক্যাটেগরি নির্বাচন করেছেন।")
     paths = categorizer.categorize(number)
-    
+    st.write(paths)
     answers = set(path[-1] for path in paths)
     if selected in answers:
         st.write(f"আপনার সংখ্যা `{selected}` ক্যাটেগরির অন্তর্ভুক্ত।")
     else:
         st.write(f"আপনার সংখ্যা `{selected}` ক্যাটেগরির অন্তর্ভুক্ত নয়।")
-    
+
     for path in paths:
         st.write(f"পথ: {path}")
         path = [(x, categories[x]) for x in path]
--- public/resources/class_10/math/real_numbers.py
+++ public/resources/class_10/math/real_numbers.py
@@ -1,30 +1,47 @@
-from sympy import nsimplify, isprime
+from sympy import isprime, nsimplify
+
 
 class RealNumbers:
     @property
     def getCategories(self):
         return {
             "বাস্তব সংখ্যা": "ব্যাখ্যা",
-            "মূলদ" : "মূলদ সংখ্যা",
-            "অমূলদ" : "অমূলদ সংখ্যা",
-            "পূর্ণ" : "পূর্ণ সংখ্যা",
-            "ভগ্নাংশ" : "ভগ্নাংশ সংখ্যা",
-            "ধনাত্মক" : "ধনাত্মক সংখ্যা",
-            "০" : "শূন্য সংখ্যা",
-            "ঋণাত্মক" : "ঋণাত্মক সংখ্যা",
-            "সাধারণ" : "সাধারণ সংখ্যা",
-            "দশমিক" : "দশমিক সংখ্যা",
-            "মৌলিক" : "মৌলিক সংখ্যা",
-            "১" : "একক সংখ্যা",
-            "যৌগিক" : "যৌগিক সংখ্যা",
-            "প্রকৃত" : "প্রকৃত সংখ্যা",
-            "অপ্রকৃত" : "অপ্রকৃত সংখ্যা",
-            "মিশ্র" : "মিশ্র সংখ্যা",
-            "সসীম" : "সসীম সংখ্যা",
-            "অসীম আবৃত্ত" : "অসীম আবৃত্ত সংখ্যা",
-            "অসীম অনাবৃত দশমিক" : "অসীম অনাবৃত দশমিক সংখ্যা",
+            "মূলদ": "মূলদ সংখ্যা",
+            "অমূলদ": "অমূলদ সংখ্যা",
+            "পূর্ণ": "পূর্ণ সংখ্যা",
+            "ভগ্নাংশ": "ভগ্নাংশ সংখ্যা",
+            "ধনাত্মক": "ধনাত্মক সংখ্যা",
+            "০": "শূন্য সংখ্যা",
+            "ঋণাত্মক": "ঋণাত্মক সংখ্যা",
+            "সাধারণ": "সাধারণ সংখ্যা",
+            "দশমিক": "দশমিক সংখ্যা",
+            "মৌলিক": "মৌলিক সংখ্যা",
+            "১": "একক সংখ্যা",
+            "যৌগিক": "যৌগিক সংখ্যা",
+            "প্রকৃত": "প্রকৃত সংখ্যা",
+            "অপ্রকৃত": "অপ্রকৃত সংখ্যা",
+            "মিশ্র": "মিশ্র সংখ্যা",
+            "সসীম": "সসীম সংখ্যা",
+            "অসীম আবৃত্ত": "অসীম আবৃত্ত সংখ্যা",
+            "অসীম অনাবৃত দশমিক": "অসীম অনাবৃত দশমিক সংখ্যা",
         }
-    
+
+    @property
+    def getFinalCategories(self):
+        return [
+            "০",
+            "ঋণাত্মক",
+            "মৌলিক",
+            "১",
+            "যৌগিক",
+            "প্রকৃত",
+            "অপ্রকৃত",
+            "মিশ্র",
+            "সসীম",
+            "অসীম আবৃত্ত",
+            "অসীম অনাবৃত দশমিক",
+        ]
+
     @property
     def getGraph(self):
         graph = {}
@@ -37,9 +54,9 @@ class RealNumbers:
         graph["সাধারণ"] = ["প্রকৃত", "অপ্রকৃত"]
         graph["অপ্রকৃত"] = ["মিশ্র"]
         graph["মিশ্র"] = ["অপ্রকৃত"]
-        graph["দশমিক"] = ["সসীম" , "অসীম আবৃত্ত"]
+        graph["দশমিক"] = ["সসীম", "অসীম আবৃত্ত"]
 
-        # চেকার 
+        # চেকার
         categories = set(self.getCategories.keys())
         covered = set(graph.keys())
         for key, values in graph.items():
@@ -49,36 +66,38 @@ class RealNumbers:
                 covered.add(value)
                 if value not in categories:
                     raise ValueError(f"Value '{value}' not found in categories.")
-        
+
         if len(covered) != len(categories):
             raise ValueError("Not all categories are covered in the graph.")
-        
+
         return graph
-    
-    def dfs(self, graph, cur, dest, path=[], visited=set()):
+
+    def dfs(self, graph, cur, dest):
+        return self.__dfs(graph, cur, dest, path=[], visited=set())
+
+    def __dfs(self, graph, cur, dest, path, visited):
         if cur not in self.getCategories or dest not in self.getCategories:
             raise ValueError(f"{cur=} or {dest=} node not found in categories.")
-        
+
         visited.add(cur)
         path.append(cur)
-
         if cur == dest:
             return path.copy()
 
         for neighbor in graph.get(cur, []):
             if neighbor not in visited:
-                result = self.dfs(graph, neighbor, dest, path, visited)
+                result = self.__dfs(graph, neighbor, dest, path, visited)
                 if result:
                     return result
 
         path.pop()
         return None
-    
+
     def is_infinite_decimal(self, value: float):
         number = nsimplify(value)
         if not number.is_rational:
             return False
-        
+
         num, denum = number.as_numer_denom()
 
         rem = num % denum
@@ -89,53 +108,53 @@ class RealNumbers:
                 return True
             rems.add(rem)
         return False
-        
 
-    def categorize(self, value:float, apostrophe=False):
+    def categorize(self, value: float, apostrophe=False):
         try:
             number = nsimplify(value)
         except (ValueError, TypeError):
             raise ValueError(f"Invalid input: {value} cannot be converted to a number.")
-        
+
+        ROOT = "বাস্তব সংখ্যা"
         paths = []
         if not number.is_rational:
-            path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "অসীম অনাবৃত দশমিক")
+            path = self.dfs(self.getGraph, ROOT, "অসীম অনাবৃত দশমিক")
             paths.append(path)
-            
+
         elif apostrophe or self.is_infinite_decimal(value):
-            path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "অসীম আবৃত্ত")
+            path = self.dfs(self.getGraph, ROOT, "অসীম আবৃত্ত")
             paths.append(path)
 
         elif number.is_Integer:
             num = int(number)
             if num < 0:
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "ঋণাত্মক")
+                path = self.dfs(self.getGraph, ROOT, "ঋণাত্মক")
                 paths.append(path)
             elif num == 0:
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "০")
+                path = self.dfs(self.getGraph, ROOT, "০")
                 paths.append(path)
             elif num == 1:
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "১")
+                path = self.dfs(self.getGraph, ROOT, "১")
                 paths.append(path)
             elif isprime(num):
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "মৌলিক")
+                path = self.dfs(self.getGraph, ROOT, "মৌলিক")
                 paths.append(path)
             else:
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "যৌগিক")
+                path = self.dfs(self.getGraph, ROOT, "যৌগিক")
                 paths.append(path)
         else:
             # it's finite fractional now
             num, denum = number.as_numer_denom()
-            if num<denum:
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "প্রকৃত") 
+            if num < denum:
+                path = self.dfs(self.getGraph, ROOT, "প্রকৃত")
                 paths.append(path)
             else:
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "অপ্রকৃত")
+                path = self.dfs(self.getGraph, ROOT, "অপ্রকৃত")
                 paths.append(path)
-                path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "মিশ্র")
+                path = self.dfs(self.getGraph, ROOT, "মিশ্র")
                 paths.append(path)
-            path = self.dfs(self.getGraph, "বাস্তব সংখ্যা", "সসীম")
+
+            path = self.dfs(self.getGraph, ROOT, "সসীম")
             paths.append(path)
 
         return paths
-
