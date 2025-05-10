4ee2e5f5e4ed7b0d21ea65b06f209fbde3f2e6b0
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-10 11:54:57 +0600

    animator has draw functionality now

--- domain/services/animate.py
+++ domain/services/animate.py
@@ -1,6 +1,8 @@
 import time
 
 import streamlit as st
+from domain.models import Point
+from PIL import ImageDraw, Image
 
 
 class Animate:
@@ -37,3 +39,17 @@ class Animate:
         """
         div = st.empty()
         self.typwritter(text, div.latex, interval)
+
+    def draw_angle(
+        self, a: Point, b: Point, c: Point, angle: float = 0, interval: float = 0.05
+    ):
+        """
+        Draw an angle between three points.
+        """
+        div = st.empty()
+        img = Image.new("RGB", (500, 500), (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+        draw.line((a.x, a.y, b.x, b.y), fill="black", width=2)
+        draw.line((b.x, b.y, c.x, c.y), fill="black", width=2)
+        draw.text((b.x + 10, b.y - 10), f"{angle:.2f}°", fill="black")
+        div.image(img)
