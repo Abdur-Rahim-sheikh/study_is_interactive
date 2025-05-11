0e367901f20c81b6e354421b35422f2d16e0ec6a
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-11 16:24:37 +0600

    adds a to positive degree

--- domain/services/angle_approximator.py
+++ domain/services/angle_approximator.py
@@ -14,7 +14,7 @@ class AngleApproximator:
     def __init__(
         self,
         background_path="public/images/geometry_background",
-        quadrant_split=1,
+        quadrant_split=4,
     ):
         if not (1 <= quadrant_split <= 4):
             raise ValueError("quadrant_split must be between 0 and 4, inclusive.")
@@ -52,26 +52,21 @@ class AngleApproximator:
         angle = math.atan2(cross_product, dot_product)
         return angle
 
-    def get_approximated_angle(self, angle: float, rad=True) -> float:
-        """
-        Returns the angle in same unit as the input angle
-        it means if rad = True, it assumes the input angle is in radians
-        and also returns the angle in radians,
-        if rad = False, it assumes the input angle is in degrees
-        and also returns the angle in degrees
-        """
+    def get_approximated_angle(
+        self, p1: Point, p2: Point, p3: Point, rad=True
+    ) -> float:
+        angle = self.__signed_angle(p1, p2, p3)
         if rad:
             block = 2 * math.pi / self.total_split
         else:
             block = 360 / self.total_split
             angle = math.degrees(angle)
-
         nearest = round(angle / block) * block
         return nearest
 
     def get_nearest_A(self, point1: Point, point2: Point, point3: Point) -> tuple:
         angle = self.__signed_angle(point1, point2, point3)
-        nearest_angle = self.get_approximated_angle(angle)
+        nearest_angle = self.get_approximated_angle(point1, point2, point3)
         delta_rad = -nearest_angle + angle
         new_point1 = self.__rotate_point(point1, point2, delta_rad)
 
@@ -115,3 +110,10 @@ class AngleApproximator:
             a, b, c = self.normalize_bc_horizontally(a, b, c)
 
         return a, b, c
+
+    def to_positive_degree(self, angle_rad: float) -> float:
+        """
+        Convert an angle in radians to a positive degree.
+        """
+        angle_degree = math.degrees(angle_rad)
+        angle_degree = (angle_degree + 360) % 360
