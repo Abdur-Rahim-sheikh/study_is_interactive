83c1f7958910a51b75fac1739dd34ff6b5968b3b
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-11 10:38:51 +0600

    pytest for angle approximator finalized

--- tests/test_angle_approximator.py
+++ tests/test_angle_approximator.py
@@ -22,82 +22,78 @@ def test_get_angle_points_returns_points():
 
 
 @pytest.mark.parametrize(
-    "a,b,c,expected_degree,quadrant_split",
+    "a,b,c,expected_a,quadrant_split",
     [
-        (Point(x=291, y=113.0), Point(x=293, y=244.0), Point(x=446.0, y=243.0), 90, 1),
-        (Point(x=204, y=73), Point(x=290.5, y=241.0), Point(x=539, y=246.0), 90, 1),
-        (Point(x=204, y=73), Point(x=290.5, y=241.0), Point(x=539, y=246.0), 135, 2),
-        (Point(x=204, y=73), Point(x=290.5, y=241.0), Point(x=539, y=246.0), 112.5, 4),
+        (
+            Point(x=291, y=113.0),
+            Point(x=293, y=244.0),
+            Point(x=446.0, y=243.0),
+            Point(x=292.14370935960466, y=112.98753201950902),
+            1,
+        ),
+        (
+            Point(x=204, y=73),
+            Point(x=290.5, y=241.0),
+            Point(x=539, y=246.0),
+            Point(x=294.3012623058589, y=52.07726339881148),
+            1,
+        ),
+        (
+            Point(x=204, y=73),
+            Point(x=290.5, y=241.0),
+            Point(x=539, y=246.0),
+            Point(x=159.59935018252128, y=104.723553475438),
+            2,
+        ),
+        (
+            Point(x=204, y=73),
+            Point(x=290.5, y=241.0),
+            Point(x=539, y=246.0),
+            Point(x=221.71430714774112, y=65.00347032161486),
+            4,
+        ),
         (
             Point(x=160.0, y=359),
             Point(x=291.0, y=243.0),
             Point(x=536.0, y=244.0),
-            180,
+            Point(x=116.02431615309038, y=242.28581353531877),
             1,
         ),
         (
             Point(x=160.0, y=359),
             Point(x=291.0, y=243.0),
             Point(x=536.0, y=244.0),
-            225,
+            Point(x=166.768501316889, y=366.22148649869547),
             2,
         ),
         (
             Point(x=160.0, y=359),
             Point(x=291.0, y=243.0),
             Point(x=536.0, y=244.0),
-            225,
+            Point(x=166.768501316889, y=366.22148649869547),
             4,
         ),
         (
             Point(x=402.0, y=339),
             Point(x=291.0, y=243.0),
             Point(x=538.0, y=244.0),
-            315,
+            Point(x=394.35040927445414, y=347.19065650432776),
             2,
         ),
     ],
 )
-def test_get_approximated_angle(a, b, c, expected_degree, quadrant_split):
+def test_get_nearest_A(a, b, c, expected_a, quadrant_split):
     approximator = AngleApproximator(quadrant_split=quadrant_split)
 
-    angle = approximator.get_approximated_angle(a, b, c, rad=False)
-    angle = (angle + 360) % 360
+    a1 = approximator.get_nearest_A(a, b, c)
 
-    angle_rad = approximator.get_approximated_angle(a, b, c, rad=True)
-    total = 2 * math.pi
-    angle_rad = (angle_rad + total) % total
-    expected_rad = math.radians(expected_degree)
-    logger.debug(f"Angle points: {a}, {b}, {c}")
-    assert pytest.approx(expected_degree, rel=1e-6) == angle
-    assert pytest.approx(expected_rad, rel=1e-6) == angle_rad
+    assert a1 == expected_a
 
 
 @pytest.mark.parametrize(
     "point, center, angle, expected",
     [
-        (Point(x=1, y=1), Point(x=0, y=0), math.pi / 2, Point(x=0, y=1.4142135623)),
-        # (Point(x=1, y=0), Point(x=0, y=0), math.pi, Point(x=-1, y=0)),
-        # (Point(x=1, y=0), Point(x=0, y=0), 3 * math.pi / 2, Point(x=0, y=1)),
-        # (Point(x=1, y=0), Point(x=0, y=0), 2 * math.pi, Point(x=1, y=0)),
-        # (
-        #     Point(x=291, y=113),
-        #     Point(x=293, y=244),
-        #     math.pi / 2,
-        #     Point(x=293, y=112.98478),
-        # ),
-        # (
-        #     Point(x=291, y=113),
-        #     Point(x=0, y=0),
-        #     math.pi / 2,
-        #     Point(x=0, y=312.1698255),
-        # ),
-        # (
-        #     Point(x=333.0, y=94.0),
-        #     Point(x=292.0, y=244.0),
-        #     math.pi / 2,
-        #     Point(x=292.0, y=93.98478),
-        # ),
+        (Point(x=1, y=1), Point(x=0, y=0), math.pi / 2, Point(x=-1, y=1)),
     ],
 )
 def test_rotate_point(point, center, angle, expected):
@@ -114,19 +110,40 @@ def test_rotate_point(point, center, angle, expected):
             Point(x=291, y=113.0),
             Point(x=293, y=244.0),
             Point(x=446.0, y=243.0),
-            Point(x=293, y=112.98478),
+            Point(x=292.14370935960466, y=112.98753201950902),
             1,
         ),
     ],
 )
-def test_get_angle_points(a, b, c, expected_a, quadrant_split):
+def test_get_angle_points_no_normalization(a, b, c, expected_a, quadrant_split):
     approximator = AngleApproximator(quadrant_split=quadrant_split)
 
     a1, b1, c1 = approximator.get_angle_points(a, b, c)
-    angle = approximator.get_approximated_angle(a, b, c)
-    assert angle == (math.pi / 2)
-
-    logger.debug(f"Angle points: {a}, {b}, {c}, {b.distance_from(a)}")
     assert b1 == b
     assert c1 == c
     assert expected_a == a1
+
+
+@pytest.mark.parametrize(
+    "a,b,c,quadrant_split",
+    [
+        (
+            Point(x=291, y=113.0),
+            Point(x=293, y=244.0),
+            Point(x=446.0, y=243.0),
+            1,
+        ),
+    ],
+)
+def test_get_angle_points_with_normalization(a, b, c, quadrant_split):
+    approximator = AngleApproximator(quadrant_split=quadrant_split)
+
+    a1, b1, c1 = approximator.get_angle_points(a, b, c, x_axis_parallel=True)
+
+    angle = approximator._AngleApproximator__signed_angle(a, b, c)
+    approximator_angle = approximator.get_approximated_angle(angle)
+
+    angle2 = approximator._AngleApproximator__signed_angle(a1, b1, c1)
+
+    assert b1.y == c1.y
+    assert approximator_angle == angle2
