4b1d439255c6ff7aa1bca048d942801c76d263d6
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-12 01:41:27 +0600

    free draw added

--- data/৩য়/গণিত/জ্যামিতি.py
+++ data/৩য়/গণিত/জ্যামিতি.py
@@ -1,63 +1,358 @@
+from datetime import datetime
+from io import BytesIO
+
 import streamlit as st
+from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
 from domain import BasePage
 from domain.models import Point
 from domain.services import AngleApproximator, Animate, StCanvasConverter
 
-allowed_distance = 5
+allowed_distance = 10
+allowed_side_diff = 20
 
 
 class GeometryPage(BasePage):
     def __init__(self):
         super().__init__(file_location=str(__file__))
-
         self.angle_approximator = AngleApproximator()
-        self.animate = Animate()
+        self.height = 400
+        self.width = 400
+        self.animate = Animate(frame_height=self.height, frame_width=self.width)
         self.canvas_converter = StCanvasConverter()
-        self.stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-
-        self.stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-        self.realtime_update = st.sidebar.checkbox("Update in realtime", True)
+        self.stroke_width = st.sidebar.slider("কলমের প্রস্থ: ", 1, 25, 3)
+        self.stroke_color = st.sidebar.color_picker("কলমের কালার: ")
+        self.bg_color = st.sidebar.color_picker("বোর্ডের কালার: ", "#eee")
+        self.help_choice = st.sidebar.pills(
+            "কোনো সাহায্য নেবেন?",
+            ["না", "আধা", "পুরা"],
+            selection_mode="single",
+            default="আধা",
+            key="help_choice",
+        )
 
     def build_page(self, **args):
-        tabs = st.tabs(["কোন", "ত্রিভুজ", "চতুর্ভুজ", "পরীক্ষা", "আঁকিবুঁকি"])
+        # tabs = st.tabs(["কোন", "ত্রিভুজ", "চতুর্ভুজ", "পরীক্ষা", "আঁকিবুঁকি"])
+        tabs = st.tabs(["কোন", "ত্রিভুজ", "আঁকিবুঁকি"])
         with tabs[0]:
             self.angle()
+            pass
+        with tabs[1]:
+            self.triangle()
+        with tabs[2]:
+            self.freedraw()
 
-    def angle(self):
-        col1, col2 = st.columns(2)
+    def triangle(self):
+        col1, col2, col3 = st.columns(3)
+        bg_image = None
+        if self.help_choice == "আধা":
+            bg_image = self.angle_approximator.get_triangle_preview(graph_style=False)
+        elif self.help_choice == "পুরা":
+            bg_image = self.angle_approximator.get_triangle_preview(graph_style=True)
 
         with col1:
+            st.write("প্রথমে `AB` এরপর `BC` শেষে `CA` বাহু আকো")
+
             canvas_result = st_canvas(
                 drawing_mode="line",
-                background_image=self.angle_approximator.get_angle_preview(),
+                background_color=self.bg_color,
+                background_image=bg_image,
                 stroke_width=self.stroke_width,
                 stroke_color=self.stroke_color,
-                update_streamlit=self.realtime_update,
+                height=self.height,
+                width=self.width,
+                key=f"{__name__}_triangle_canvas",
             )
 
-            st.write(canvas_result.json_data)
         with col2:
-            points = self.__extract_points_from_line(canvas_result.json_data)
-            if len(points) != 4:
-                st.stop()
+            title = st.empty()
+            approximated_angle = None
+            points = self.__extract_angle_from_lines(canvas_result.json_data)
+            if len(points) > 6:
+                st.error("আপনি ৩ টির বেশি রেখা এঁকেছেন")
+                return
+            elif len(points) != 6:
+                title.write("প্রতিচ্ছবি :sunglasses:")
+                startpoints = [points[i] for i in range(0, len(points), 2)]
+                endpoints = [points[i] for i in range(1, len(points), 2)]
+                self.animate.draw_lines(
+                    startpoints,
+                    endpoints,
+                    stroke_width=self.stroke_width,
+                    stroke_color=self.stroke_color,
+                )
+                return
+
+            status, points = self.__try_merge_vertices(points)
+            if not status:
+                st.error("সংযোগ বিন্দু আরো কাছাকাছি আনুন")
+                return
+
+            p1, p2, p3 = self.angle_approximator.get_angle_points(
+                points[0], points[1], points[2]
+            )
+            approximated_angle = self.angle_approximator.get_approximated_angle(
+                p1, p2, p3, rad=False
+            )
+
+            title.write("আনুমানিক প্রদর্শন :brain:")
+            self.animate.draw_angle(
+                [
+                    p1,
+                    p2,
+                    p3,
+                ],
+                polygon=True,
+                show_side_diff=True,
+                allowed_diff=allowed_side_diff,
+                stroke_width=self.stroke_width,
+                stroke_color=self.stroke_color,
+            )
+        with col3:
+            if not approximated_angle:
+                st.write("এখনো কোন ত্রিভুজ আকা হয়নি")
+                return
+            answer = st.pills(
+                "বলতে পারো কি ত্রিভুজ একেছো?",
+                ["সমবাহু", "সমদ্বিবাহু", "বিষমবাহু"],  # এই নামের সাথে ম্যাচ করা হবে
+                selection_mode="single",
+            )
+            if answer and st.button("উত্তর মিলাই", key="triangle"):
+                status, message = self.__match_triangle_answer(
+                    p1, p2, p3, answer, allowed_diff=allowed_side_diff
+                )
+                if status:
+                    st.balloons()
+                    self.animate.write(message)
+
+                else:
+                    self.animate.write(message)
+
+    def angle(self):
+        col1, col2, col3 = st.columns(3)
+        bg_image = None
+        if self.help_choice == "আধা":
+            bg_image = self.angle_approximator.get_angle_preview(graph_style=False)
+        elif self.help_choice == "পুরা":
+            bg_image = self.angle_approximator.get_angle_preview(graph_style=True)
+        with col1:
+            st.write("প্রথমে `AB` এরপর `BC` বাহু আকো")
+
+            canvas_result = st_canvas(
+                drawing_mode="line",
+                background_color=self.bg_color,
+                background_image=bg_image,
+                stroke_width=self.stroke_width,
+                stroke_color=self.stroke_color,
+                height=self.height,
+                width=self.width,
+                key="angle_canvas",
+            )
+
+        with col2:
+            title = st.empty()
+            approximated_angle = None
+            points = self.__extract_angle_from_lines(canvas_result.json_data)
+            if len(points) > 4:
+                st.error("আপনি ২টির বেশি রেখা এঁকেছেন")
+                return
+            elif len(points) != 4:
+                title.write("প্রতিচ্ছবি :sunglasses:")
+                startpoints = [points[i] for i in range(0, len(points), 2)]
+                endpoints = [points[i] for i in range(1, len(points), 2)]
+                self.animate.draw_lines(
+                    startpoints,
+                    endpoints,
+                    stroke_width=self.stroke_width,
+                    stroke_color=self.stroke_color,
+                )
+                return
             dist = points[1].distance_from(points[2])
             if dist > allowed_distance:
-                st.error("দূরে")
-                st.stop()
+                st.error("সংযোগ বিন্দু আরো কাছাকাছি আনুন")
+                return
+
             middle_x = (points[1].x + points[2].x) / 2
             middle_y = (points[1].y + points[2].y) / 2
             middle = Point(middle_x, middle_y)
             p1, p2, p3 = self.angle_approximator.get_angle_points(
                 points[0], middle, points[3]
             )
-            st.write(f"{points[0]=} {middle=} {points[3]=}")
+            approximated_angle = self.angle_approximator.get_approximated_angle(
+                p1, p2, p3, rad=False
+            )
 
-            self.animate.draw_angle(p1, p2, p3)
+            title.write("আনুমানিক প্রদর্শন :brain:")
+            self.animate.draw_angle(
+                [
+                    p1,
+                    p2,
+                    p3,
+                ],
+                stroke_width=self.stroke_width,
+                stroke_color=self.stroke_color,
+            )
+        with col3:
+            if not approximated_angle:
+                st.write("এখনো কোন কোণ আকা হয়নি")
+                return
+            answer = st.pills(
+                "বলতে পারো কি কোন একেছো?", ["সূক্ষ্মকোণ", "সমকোন", "সরলকোণ", "স্থূলকোণ"]
+            )
+            if answer and st.button("উত্তর মিলাই"):
+                status, message = self.__match_angle_answer(approximated_angle, answer)
+                if status:
+                    st.balloons()
+                    self.animate.write(message)
 
-    def __extract_points_from_line(self, json_data) -> list[Point]:
-        if json_data is None or len(json_data["objects"]) > 2:
+                else:
+                    self.animate.write(message)
+
+    def freedraw(self):
+        col1, col2 = st.columns([2, 5], gap="large")
+        width = 600
+        height = 400
+
+        with col1:
+            drawing_mode = st.selectbox(
+                "ড্রয়িং টুল:",
+                ("point", "freedraw", "line", "rect", "circle", "transform"),
+                index=1,
+            )
+
+            point_display_radius = 3
+            if drawing_mode == "point":
+                point_display_radius = st.slider("বিন্দুর ব্যস্যার্ধ: ", 1, 25, 3)
+            bg_image = st.file_uploader("ব্যাকগ্রাউন্ডের ছবি:", type=["png", "jpg"])
+            if bg_image:
+                bg_image = Image.open(bg_image)
+        with col2:
+            canvas_result = st_canvas(
+                fill_color="rgba(255, 165, 0, 0.3)",
+                drawing_mode=drawing_mode,
+                background_color=self.bg_color,
+                background_image=bg_image if bg_image else None,
+                stroke_width=self.stroke_width,
+                stroke_color=self.stroke_color,
+                width=width,
+                height=height,
+                point_display_radius=point_display_radius,
+                key=f"{__file__}_freedraw_canvas",
+            )
+
+        # if bg_image and st.button("ব্যাকগ্রাউন্ড সরান", icon=":material/delete_forever:"):
+        #     bg_image = None
+        #     st.rerun()
+
+        if canvas_result.image_data is not None:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            file_name = f"interactive_study_{timestamp}.png"
+            drawn_img = Image.fromarray((canvas_result.image_data).astype("uint8"))
+
+            if bg_image:
+                background = bg_image.convert("RGBA").resize((width, height))
+                combined_img = Image.alpha_composite(background, drawn_img)
+            else:
+                combined_img = drawn_img
+            # Save to in-memory file
+            buffer = BytesIO()
+            combined_img.save(buffer, format="PNG")
+            buffer.seek(0)
+            st.download_button(
+                label="ডাউনলোড করি",
+                data=buffer,
+                file_name=file_name,
+                mime="image/png",
+                icon=":material/download:",
+            )
+
+    def __match_angle_answer(self, approximated_deg, given_answer):
+        approximated_deg = (approximated_deg + 360) % 360
+        if approximated_deg > 180:
+            approximated_deg = 360 - approximated_deg
+        data = ""
+        if approximated_deg < 90:
+            answer = "সূক্ষ্মকোণ"
+            data = (
+                f"{approximated_deg}° কোণটি এক সমকোণের চেয়ে ছোট তাই একে `সূক্ষ্মকোণ` বলা হয়"
+            )
+        elif approximated_deg == 90:
+            answer = "সমকোন"
+            data = f"{approximated_deg}° কোণটি একটি এক `সমকোন`"
+        elif approximated_deg == 180:
+            answer = "সরলকোণ"
+            data = f"{approximated_deg}° কোণটি দুই সমকোণের সমান তাই একে `সরলকোণ` বলা হয়"
+        else:
+            answer = "স্থূলকোণ"
+            data = f"{approximated_deg}° কোণটি এক সমকোণের চেয়ে বড় তাই একে `স্থূলকোণ` বলা হয়"
+
+        status = True
+        message = f"হুররাহ! সঠিক উত্তর এটি একটি `{answer}`!"
+        if answer != given_answer:
+            status = False
+            message = f"দুঃখিত! তোমার {given_answer} উত্তর টি ভুল! এটি একটি `{answer}`!"
+
+        message = f"{message}\n\n{data}"
+
+        return status, message
+
+    def __match_triangle_answer(
+        self, p1: Point, p2: Point, p3: Point, given_answer: str, allowed_diff: int
+    ):
+        ab = p1.distance_from(p2)
+        bc = p2.distance_from(p3)
+        ca = p3.distance_from(p1)
+        angle_deg = self.angle_approximator.get_approximated_angle(
+            p1, p2, p3, rad=False
+        )
+
+        angle_msg = "পাশাপাশি এটি একটি `সূক্ষ্মকোণী` ত্রিভুজও বটে"
+
+        if angle_deg == 90:
+            angle_msg = "পাশাপাশি এটি একটি `সমকোণী` ত্রিভুজও বটে"
+        elif angle_deg > 90:
+            angle_msg = "পাশাপাশি এটি একটি `স্থূলকোণী` ত্রিভুজও বটে"
+
+        triangle = "বিষমবাহু"
+
+        def allowed(a, b):
+            return abs(a - b) <= allowed_diff
+
+        if allowed(ab, bc) and allowed(bc, ca):
+            triangle = "সমবাহু"
+        elif allowed(ab, bc) or allowed(bc, ca) or allowed(ca, ab):
+            triangle = "সমদ্বিবাহু"
+
+        status = True
+        answer_msg = "হুররাহ! তোমার উত্তরটি সঠিক হয়েছে"
+        if given_answer != triangle:
+            status = False
+            answer_msg = "দুঃখিত! তোমার উত্তরটি ভুল"
+
+        triangle_msg = f"এটি একটি `{triangle}` ত্রিভুজ"
+        msg = f"- {answer_msg} \n- {triangle_msg} এবং {angle_msg}"
+
+        return status, msg
+
+    def __try_merge_vertices(self, points: list[Point], allowed_distance: int = 5):
+        if len(points) % 2 != 0:
+            raise ValueError(
+                "points should come in even as to make a line we need pair"
+            )
+
+        answers = []
+        for i in range(0, len(points), 2):
+            if points[i].distance_from(points[i - 1]) > allowed_distance:
+                return False, []
+
+            x1 = (points[i].x + points[i - 1].x) / 2
+            y1 = (points[i].y + points[i - 1].y) / 2
+            answers.append(Point(x1, y1))
+        return True, answers
+
+    def __extract_angle_from_lines(self, json_data) -> list[Point]:
+        if json_data is None:
             return []
         points = []
         for obj in json_data["objects"]:
--- domain/services/animate.py
+++ domain/services/animate.py
@@ -1,3 +1,4 @@
+import colorsys
 import time
 
 import streamlit as st
@@ -7,6 +8,26 @@ from domain.models import Point
 
 
 class Animate:
+    BASE_COLORS = [
+        "#000000",  # Black
+        "#e6194b",  # Red
+        "#3cb44b",  # Green
+        "#ffe119",  # Yellow
+        "#0082c8",  # Blue
+        "#f58231",  # Orange
+        "#911eb4",  # Purple
+        "#46f0f0",  # Cyan
+        "#f032e6",  # Pink
+        "#d2f53c",  # Lime
+        "#fabebe",  # Light Pink
+        "#008080",  # Teal
+        "#e6beff",  # Lavender
+        "#aa6e28",  # Brown
+        "#fffac8",  # Light Yellow
+        "#800000",  # Maroon
+        "#aaffc3",  # Mint
+    ]
+
     def __init__(self, frame_width: int = 600, frame_height: int = 400):
         self.width = frame_width
         self.height = frame_height
@@ -47,10 +68,10 @@ class Animate:
 
     def draw_angle(
         self,
-        a: Point,
-        b: Point,
-        c: Point,
-        angle: float = 0,
+        points: list[Point],
+        polygon: bool = False,
+        show_side_diff: bool = False,
+        allowed_diff: int = None,
         height: int = None,
         width: int = None,
         stroke_width: int = 3,
@@ -58,48 +79,40 @@ class Animate:
         duration: float = 0.5,
     ):
         """
-        Draw an angle between three points.
+        - Draw an angle between three points.
+        - need to provide `allowed_diff` if `show_side_diff` is True
         """
         if height is None:
             height = self.height
         if width is None:
             width = self.width
+
+        colors = [stroke_color] * len(points)
+        text_fill = "black"
+        if show_side_diff:
+            colors = self.__choose_side_colors(points=points, allowed_diff=allowed_diff)
+
         div = st.empty()
         img = Image.new("RGB", (width, height), (255, 255, 255))
         draw = ImageDraw.Draw(img)
-        draw.line((a.x, a.y, b.x, b.y), fill=stroke_color, width=stroke_width)
-        draw.line((b.x, b.y, c.x, c.y), fill=stroke_color, width=stroke_width)
-        # bc = b.distance_from(c)
-        # ab = a.distance_from(b)
-        # radius = 0.1 * min(bc, ab)
-        # start_angle = a.angle_to(b)
-        # end_angle = c.angle_to(b)
-        draw.arc([a.x, a.y, c.x, c.y], 0, angle, fill=stroke_color, width=stroke_width)
-        draw.text(
-            (b.x + 5, b.y - 5),
-            "θ",
-            fill="black",
-            font=None,
-        )
-        draw.text(
-            (a.x + 5, a.y - 5),
-            "A",
-            fill="black",
-            font=None,
-        )
-        draw.text(
-            (b.x + 5, b.y + 5),
-            "B",
-            fill="black",
-            font=None,
-        )
 
-        draw.text(
-            (c.x + 5, c.y + 5),
-            "C",
-            fill="black",
-            font=None,
-        )
+        for idx in range(len(points) - 1):
+            a, b = points[idx], points[idx + 1]
+            draw.line((a.x, a.y, b.x, b.y), fill=colors[idx], width=stroke_width)
+            angle_name = chr(ord("A") + idx)
+            draw.text((a.x + 5, a.y - 5), text=angle_name, fill=text_fill)
+
+        last = points[-1]
+        angle_name = chr(ord("a") + len(points) - 1)
+        draw.text((last.x + 5, last.y - 5), text=angle_name, fill=text_fill)
+        if polygon:
+            first = points[0]
+            draw.line(
+                (last.x, last.y, first.x, first.y),
+                fill=colors[-1],
+                width=stroke_width,
+            )
+
         div.image(img)
 
     def draw_lines(
@@ -133,3 +146,36 @@ class Animate:
                 width=stroke_width,
             )
         div.image(img)
+
+    def __choose_side_colors(
+        self,
+        points: list[Point],
+        allowed_diff: int,
+    ) -> list:
+        n = len(points)
+        sides = [points[(i + 1) % n].distance_from(points[i]) for i in range(n)]
+
+        def allowed(a, b):
+            return abs(a - b) < allowed_diff
+
+        groups = []
+        for idx, length in enumerate(sides):
+            placed = False
+
+            for group in groups:
+                if allowed(group[0][1], length):
+                    group.append((idx, length))
+                    placed = True
+                    break
+
+            if not placed:
+                groups.append([(idx, length)])
+
+        side_colors = [None] * len(sides)
+
+        for idx, group in enumerate(groups):
+            color = self.BASE_COLORS[idx % len(self.BASE_COLORS)]
+            for idx, _ in group:
+                side_colors[idx] = color
+
+        return side_colors
