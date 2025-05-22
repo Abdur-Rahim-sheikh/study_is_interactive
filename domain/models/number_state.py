5ce8878185c7b600254bd554427fe38205bec620
Abdur-Rahim-sheikh <abi.rahim.sheikh@gmail.com>
2025-05-22 20:12:08 +0600

    backbone

--- domain/models/number_state.py
+++ domain/models/number_state.py
@@ -3,7 +3,48 @@ from dataclasses import dataclass
 
 @dataclass
 class NumberState:
-    first_operand: int
-    second_operand: int
-    result: int
-    after_decimal: bool = False
+    """
+    All the variable keeps in decimal format, we format later using formatter
+    """
+
+    from_base: int
+    to_base: int
+    decimal_result: float
+    decimal_partial: float = None
+
+    def get_converter(self):
+        """
+        Get converter
+        """
+        convert = None
+        if self.to_base == "binary":
+
+            def convert(x):
+                return bin(x)[2:]
+        elif self.to_base == "octal":
+
+            def convert(x):
+                return oct(x)[2:]
+        elif self.to_base == "decimal":
+
+            def convert(x):
+                return str(x)
+        else:
+
+            def convert(x):
+                return hex(x)[2:]
+
+        return convert
+
+    def get_formated(self) -> dict:
+        """
+        Get formatted dict
+        """
+        convert = self.get_converter()
+
+        return {
+            "from_base": self.from_base,
+            "to_base": self.to_base,
+            "result": convert(self.decimal_result),
+            "partial": convert(self.decimal_result) if self.decimal_partial else None,
+        }
--- domain/services/number_converter.py
+++ domain/services/number_converter.py
@@ -1,25 +1,28 @@
+from ..models import NumberState
+
+
 class NumberConverter:
     NUMBER_TYPES = {
-        "বাইনারি": "binary",
-        "অক্ট্যাল": "octal",
-        "দশমিক": "decimal",
-        "হেক্সাডেসিমেল": "hexadecimal",
+        "binary": "বাইনারি",
+        "octal": "অক্ট্যাল",
+        "decimal": "দশমিক",
+        "hexadecimal": "হেক্সাডেসিমেল",
     }
 
-    @staticmethod
-    def get_available_names(self) -> dict:
+    def get_available_bases(self) -> dict:
         return self.NUMBER_TYPES
 
     def get_permitted_digits(self, base: int) -> set:
         # fmt: off
         allowed = [".","0", "1","2", "3", "4", "5", "6","7", "8", 
-                   "9","A", "B", "C", "D", "E", "F"],
-        # fmt: onn
-        return set(allowed[:base+1])
+                   "9","A", "B", "C", "D", "E", "F"]
+        # fmt: on
+        return set(allowed[: base + 1])
 
     def valid(self, number: str, base: int):
         allowed = self.get_permitted_digits(base)
-        return all(ch in allowed for ch in str)
+        number = str(number)
+        return all(ch in allowed for ch in number) and number.count(".") <= 1
 
     def get_base(self, type: str):
         base = None
@@ -33,8 +36,71 @@ class NumberConverter:
             base = 16
         return base
 
-    def convert(self, number: str, convert_from: str, convert_to: str) -> str:
+    def convert_to_decimal(self, number: str, base: int) -> NumberState:
+        idx = number.index(".") if "." in number else len(number)
+        idx -= 1
+        decimal = 0
+        for ch in number:
+            if ch == ".":
+                continue
+            decimal += int(ch, base) * (base**idx)
+            idx -= 1
+        state = NumberState(
+            from_base=base,
+            to_base=10,
+            decimal_result=decimal,
+        )
+        return state
+
+    def convert_from_decimal(self, number: str, base: int) -> list[NumberState]:
+        int_part = int(number)
+        decimal_part = float(number) - int_part
+        states = []
+
+        while int_part > 0:
+            remainder = int_part % base
+            int_part = int_part // base
+            states.append(
+                NumberState(
+                    from_base=10,
+                    to_base=base,
+                    decimal_result=int_part,
+                    decimal_partial=remainder,
+                )
+            )
+
+        mx = 10
+        while decimal_part > 0 and mx > 0:
+            decimal_part *= base
+            int_part = int(decimal_part)
+            decimal_part -= int_part
+            states.append(
+                NumberState(
+                    from_base=10,
+                    to_base=base,
+                    decimal_result=decimal_part,
+                    decimal_partial=int_part,
+                )
+            )
+            mx -= 1
+        return states
+
+    def convert(
+        self, number: str, convert_from: str, convert_to: str
+    ) -> list[NumberState]:
         base1, base2 = self.get_base(convert_from), self.get_base(convert_to)
 
         if not self.valid(number, base1):
             raise ValueError(f"{number} টি {convert_from} বেস ফরম্যাটে নেই")
+
+        states = []
+        if base1 != 10:
+            state = self.convert_to_decimal(number, base1)
+            number = state.decimal_result
+            states.append(state)
+
+        if base2 != 10:
+            next_states = self.convert_from_decimal(number, base2)
+            states.extend(next_states)
+
+        return states
